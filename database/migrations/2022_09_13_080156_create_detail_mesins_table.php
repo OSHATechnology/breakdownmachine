07f024f0f5ad7cba47d79bac9d6a979638d6cdd0
OSHATechnology <oshatechnology@gmail.com>
2022-09-13 15:18:27 +0700

    table update

---
+++ database/migrations/2022_09_13_080156_create_detail_mesins_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_mesins', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_nama');
+            $table->bigInteger('id_jenis');
+            $table->string('type',20);
+            $table->date('latest_maintenance');
+            $table->date('next_maintenance');
+            $table->string('condition',15);
+            $table->integer('breakdown_possibility');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_mesins');
+    }
+};
