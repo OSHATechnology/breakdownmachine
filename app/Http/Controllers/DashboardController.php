d9fcf4dd45fa4d05db78c9094cc34e0a345bf2e4
OSHATechnology <oshatechnology@gmail.com>
2022-09-22 11:47:34 +0700

    database seeder

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,21 +2,55 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\CategoryMesin;
 use App\Models\DetailMesin;
+use App\Models\JenisMesin;
 use App\Models\Mesin;
 use Illuminate\Http\Request;
 use DB;
-
+use Illuminate\Support\Facades\DB as FacadesDB;
 
 class DashboardController extends Controller
 {
-    public function index(){
-        $mesin = Mesin::all();
-        $dmesin = DetailMesin::all();
-        $cmesin = CategoryMesin::all();
+     public function index(){
+          $mesin = Mesin::all();
+          $jmesin = JenisMesin::all();
+          $dmesin = DetailMesin::all();
+          $date = date_create($dmesin->min('next_maintenance'));
 
-        // dd($dmesin);
-        return view('components.dashboard',compact('mesin','dmesin','cmesin'));
+          $data_possibility = DB::table('detail_mesins')
+                    ->select('breakdown_possibility', DB::raw('count(breakdown_possibility) as total'))
+                    ->groupBy('breakdown_possibility')
+                    ->get();
+          
+          $data_maintenance = DB::table('detail_mesins')
+                    ->select('next_maintenance', DB::raw('count(next_maintenance) as total'))
+                    ->groupBy('next_maintenance')
+                    ->get();
+
+          $format_Date = date_format($date, 'd M Y');
+          return view('components.dashboard',compact('mesin','jmesin','dmesin', 'format_Date', 'data_possibility', 'data_maintenance'));
+    }
+
+    public function search($word)
+    {
+        $data = DetailMesin::query()
+                ->join('mesins', 'mesins.id', '=', 'detail_mesins.id_nama')
+                ->join('jenis_mesins', 'jenis_mesins.id', '=', 'detail_mesins.id_jenis')
+                ->where('detail_mesins.kode_mesin', 'LIKE', "%$word%")
+                ->orWhere('detail_mesins.nama', 'LIKE', "%$word%")
+                ->first();
+
+        
+        echo json_encode($data);
+    }
+
+    public function chartDonutPossibility()
+    {
+          $data_possibility = DB::table('detail_mesins')
+               ->select('breakdown_possibility', DB::raw('count(breakdown_possibility) as total'))
+               ->groupBy('breakdown_possibility')
+               ->get();
+
+          echo json_encode($data_possibility);
     }
 }
